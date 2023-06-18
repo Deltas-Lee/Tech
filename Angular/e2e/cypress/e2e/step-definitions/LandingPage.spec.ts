caaa8c0f4c6c031cc871145b9ac98a04ff7fbbc5
Deltas Lephalala <61046765+Deltas-Lee@users.noreply.github.com>
2023-06-18 14:36:31 +0200

    Add changes to select hero and display their details

--- Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
+++ Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
@@ -1,9 +1,17 @@
 import { Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
-
+import { Navigate} from "../page-obj/navigate";
 Given(/^the user is on the landing page$/, () => {
 	cy.visit("http://localhost:4200/dashboard");
 });
 
 Then(/^the user shall see the employee dashboard$/, () => {
-	return true;
+	Navigate.employeeDashboard();
+});
+
+Then(/^the user shall see the top 4 Heroes displayed$/, () => {
+	Navigate.listOfTopHeros();
+});
+
+Then(/^the user shall select a hero from the dashboard and see the hero details displayed$/, () => {
+	Navigate.viewHeroDetails();
 });
