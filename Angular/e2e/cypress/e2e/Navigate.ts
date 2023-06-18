b3bc26dbe219a12565633143a29396b5445da388
Deltas Lephalala <61046765+Deltas-Lee@users.noreply.github.com>
2023-06-18 14:40:50 +0200

    Create page obj methods

---
+++ Angular/e2e/cypress/e2e/Navigate.ts
@@ -0,0 +1,12 @@
+export class Navigate {
+  static employeeDashboard() {
+    cy.get('h2').contains('Top Heroes');
+  }
+  static listOfTopHeros() {
+    cy.get('.heroes-menu > a').should('have.length', 4);
+  }
+  static viewHeroDetails() {
+    cy.get('.heroes-menu > a:first-child').click();
+    cy.get('label').contains('Hero name:');
+  }
+}
